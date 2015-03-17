68a11467f35daaacc48420aa94c67b1abc324e82
Ben <ben.simmons@trinityis.com>
2015-03-17 12:38:52 -0500

    Completed strings

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,26 +12,37 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSString *favoriteCheeseString = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    return favoriteCheeseString ;
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSRange suffixRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    NSString *justCheese = nil;
+    if (suffixRange.location != NSNotFound) {
+        justCheese = [cheeseName stringByReplacingCharactersInRange:suffixRange withString:@""];
+    } else {
+        justCheese = cheeseName;
+    }
+    return justCheese;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+    NSString *cheeseString = nil;
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        cheeseString = [NSString stringWithFormat:@"%lu cheese", (unsigned long) cheeseCount];
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        cheeseString = [NSString stringWithFormat:@"%lu cheeses", (unsigned long) cheeseCount];
     }
     
     /*
      (You will learn more about if/else statements in the next checkpoint.)
      */
     
-    return nil;
+    return cheeseString;
 }
 
 @end
